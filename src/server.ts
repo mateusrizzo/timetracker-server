d9b493178e8b988d3fcd38631c62bf95befc159e
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-19 11:58:09 -0300

    added typing to user create service

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-
 import 'reflect-metadata';
 
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(routes);
 
 app.listen(3333, () => {console.log("Server online")});
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -3,9 +3,15 @@ import {hash} from 'bcryptjs';
 
 import User from '../models/User';
 
+interface Request {
+    name: string;
+    email: string;
+    password: string;
+}
+
 
 export default class CreateUserService {
-   public async execute ({name, email, password}) {
+   public async execute ({name, email, password}: Request): Promise<User> {
         const userRepository = getRepository(User);
 
         const checkIfUserExists = await userRepository.findOne({
