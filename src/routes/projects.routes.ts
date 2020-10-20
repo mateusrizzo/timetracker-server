3c27739cbc235fec7269818bcb368e309319fc33
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-20 16:03:54 -0300

    Create user logic added

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -2,10 +2,20 @@ import {Router} from 'express';
 
 import testAuthentication from '../middleware/testAuthentication';
 
+import CreateProjectService from '../services/CreateProjectService';
+
 const projectsRoutes = Router();
 
 projectsRoutes.post('/', testAuthentication, async (request, response) => {
-    
+
     const {name} = request.body;
 
+    const createProjectService = new CreateProjectService();
+
+    const project = createProjectService.execute(name);
+
+    return response.json(project);
+
 })
+
+export default projectsRoutes;
--- src/services/CreateProjectService.ts
+++ src/services/CreateProjectService.ts
@@ -4,6 +4,14 @@ import Project from '../models/Project';
 
 export default class CreateProjectService {
     public async execute({name}): Promise<Project> {
+        const projectsRepository = getRepository(Project);
 
+        const project = await projectsRepository.create({
+            name
+        });
+
+        await projectsRepository.save(project);
+
+        return project;
     }
 }
