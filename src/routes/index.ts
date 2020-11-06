c175946b710530b324792716eb0fbf97703ec158
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-11-06 11:07:53 -0300

    Added basic create session route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import {Router} from 'express';
 import usersRouter from './users.routes';
 import loginRouter from './login.routes';
 import projectsRouter from './projects.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/auth', loginRouter);
 routes.use('/projects', projectsRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -3,3 +3,10 @@ import {Router} from 'express';
 import testAuthentication from '../middleware/testAuthentication';
 
 const sessionsRouter = Router();
+
+sessionsRouter.post('/:id', testAuthentication, async(request, response) => {
+    const {minutes, seconds} = request.body;
+    const {project_id} = request.params;
+});
+
+export default sessionsRouter;
