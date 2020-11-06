08e2a3cd8745b1e809fd2622753f4e5e70941184
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-11-06 13:58:17 -0300

    Added session listing route

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import { getCustomRepository } from 'typeorm';
 
 import testAuthentication from '../middleware/testAuthentication';
 import SessionsRepository from '../repositories/SessionsRepository';
@@ -6,6 +7,15 @@ import SessionsRepository from '../repositories/SessionsRepository';
 import CreateSessionService from '../services/CreateSessionService';
 
 const sessionsRouter = Router();
+
+sessionsRouter.get('/:id', testAuthentication, async(request, response) => {
+    const {id} = request.params;
+
+    const sessionsRepository = getCustomRepository(SessionsRepository);
+    const sessions = await sessionsRepository.findByProject({project_id: id});
+
+    return response.json(sessions);
+});
 
 sessionsRouter.post('/:id', testAuthentication, async(request, response) => {
     const {minutes, seconds} = request.body;
