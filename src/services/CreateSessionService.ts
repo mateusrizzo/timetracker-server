9cdd8bd054f9a935f27a95152a8c2b15d7049fa2
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-11-06 11:16:09 -0300

    Removed unused import

--- src/services/CreateSessionService.ts
+++ src/services/CreateSessionService.ts
@@ -2,8 +2,6 @@ import {getRepository} from 'typeorm';
 
 import Session from '../models/Session';
 
-import AppError from '../errors/AppError';
-
 export default class CreateSessionService {
     public async execute ({minutes, seconds, project_id}) {
         const sessionsRepository = getRepository(Session);
